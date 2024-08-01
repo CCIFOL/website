d458eaf4d7361aac94413d0bc8e3bbf56ea4fb6b
tee <tee@Kinyi.Kinyi>
2024-08-01 12:36:18 +0300

    commiting to the repo

--- backend/config.js
+++ backend/config.js
@@ -5,13 +5,15 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
+    mongoose.set('strictQuery', true);
+    mongoose
+        .connect(process.env.MONGO_URI)
+        .then(() => console.log('Connected to MongoDB'))
+        .catch((err) => console.error('Could not connect to MongoDB', err));
+
+    console.log('Connected to MongoDB');
   } catch (err) {
-    console.error(err.message);
+    console.error('Could not connect to MongoDB', err);
     process.exit(1); // Exit process with failure
   }
 };
--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,19 @@ const connectDB = require('./config');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config();
 
 const app = express();
 
+// Connect to MongoDB
 connectDB();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
