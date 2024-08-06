1349631dce587c2fee650dd34c2d07b871b0f95d
tee <tee@Kinyi.Kinyi>
2024-08-06 23:44:33 +0300

    commiting to the repo

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -28,7 +28,7 @@ exports.createEvent = async (req, res) => {
 // Delete an event
 exports.deleteEvent = async (req, res) => {
   try {
-    const event = await Event.findOne({ date: req.params.id }); 
+    const event = await Event.findOne({ title: req.params.id }); 
     if (!event) {
       return res.status(404).json({ message: 'Event not found' });
     }
