0008167d0ce0fe9c31143bb90a4ebc0c19db81de
Kinyi <kinyi9461@gmail.com>
2025-05-15 22:09:01 +0300

    commiting to the repo

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 console.log("wewewe");
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
